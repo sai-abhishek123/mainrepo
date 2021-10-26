49dbdd52e43ffe9330cd8bb662b432d2920042bb
Sai Abhishek <sai.abhishek905@gmail.com>
2021-10-26 20:15:45 +0300

    654

--- Cp1.c
+++
@@ -1,8 +0,0 @@
-#include<iostream.h>
-main()
-{
-    int i,b;
-    scanf("%d%d",&i,&b);
-    int c=i+b;
-    printf("%d",c);
-}
--- Cp1.py
+++
@@ -1,4 +0,0 @@
-a=int(input("Hello: Please enter a number: "))
-b=int(input("Please enter another number: "))
-c=a+b
-print("This is the sum you have requested: "+c)
