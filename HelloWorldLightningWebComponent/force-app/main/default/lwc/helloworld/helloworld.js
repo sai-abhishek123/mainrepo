48424b33d9ba26952ab526da795a8b5141b4ddae
Sai Abhishek <sai.abhishek905@gmail.com>
2021-11-30 18:33:33 +0300

    lwc

---
+++ HelloWorldLightningWebComponent/force-app/main/default/lwc/helloworld/helloworld.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+export default class Helloworld extends LightningElement {
+    greeting = "World";
+    changehandler(event)
+    {
+        this.greeting=event.target.value;
+    }
+}
